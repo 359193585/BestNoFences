fd110b72981d48c5fe7d6dc1f0259e32a0d37694
leison <359193585@qq.com>
2026-03-30 14:45:48 +0800

    fix: Added multi - language support for the context menu.

--- BestNoFences/FenceWindow.Designer.cs
+++ BestNoFences/FenceWindow.Designer.cs
@@ -108,9 +108,10 @@
             // 
             // settingsToolStripMenuItem
             // 
+            resources.ApplyResources(this.settingsToolStripMenuItem, "settingsToolStripMenuItem");
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.settingsToolStripMenuItem.Text = "Settings";
+            //this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            //this.settingsToolStripMenuItem.Text = "Settings";
             this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
             // 
             // renameToolStripMenuItem
